5b50b46f7471e05e9a8f6d4af1d09cbcdee0e8d0
Andreas Monsch <amo@solute.de>
2018-06-15 10:56:45 +0200

    Add completion

--- pt_esql.py
+++ pt_esql.py
@@ -8,11 +8,23 @@ from pygments.lexers import SqlLexer
 from prompt_toolkit import prompt
 from prompt_toolkit.lexers import PygmentsLexer
 from prompt_toolkit.shortcuts import PromptSession
+from prompt_toolkit.completion import WordCompleter
+
+
+sql_completer =  WordCompleter(
+    [
+        "SELECT", "FROM", "WHERE", "ORDER BY", "AND", "OR",
+    ],
+    ignore_case=True,
+)
+
 
 def main():
     session = PromptSession(
         ">",
         lexer=PygmentsLexer(SqlLexer),
+        completer=sql_completer,
+        complete_while_typing=False,
     )
 
     while True:
