2724625c952e9470549175cb1fa1ce1561c0297f
L3viathan <git@l3vi.de>
2018-06-15 22:11:27 +0200

    Move SQL functionality to seperate module

--- pt_esql.py
+++ pt_esql.py
@@ -5,20 +5,16 @@ Demonstration of how the input can be indented.
 
 import json
 import subprocess
-from contextlib import redirect_stdout
 from io import BytesIO
 
 import click
 import moz_sql_parser
-import pyparsing
 import Levenshtein
 import elasticsearch
 
-from pygments.lexers import SqlLexer, JsonLexer
+from pygments.lexers import JsonLexer
 from pygments.styles import get_style_by_name
-from pygments.token import Token
 
-from prompt_toolkit import prompt
 from prompt_toolkit.renderer import (
     print_formatted_text as renderer_print_formatted_text
 )
@@ -26,60 +22,16 @@ from prompt_toolkit.output.vt100 import Vt100_Output
 from prompt_toolkit.styles.pygments import style_from_pygments_cls
 from prompt_toolkit.lexers import PygmentsLexer
 from prompt_toolkit.shortcuts import PromptSession, print_formatted_text
-from prompt_toolkit.completion import Completer, Completion
 from prompt_toolkit.history import FileHistory
-from prompt_toolkit.validation import Validator, ValidationError
 from prompt_toolkit.formatted_text import HTML, PygmentsTokens
 from prompt_toolkit.key_binding import KeyBindings
+
+from sql import SQLLexer, SQLValidator, SQLCompleter, sql_keywords
 
 
 def get_size():
     x, y = subprocess.check_output(["stty", "size"]).decode("utf8").split()
     return int(x), int(y)
-
-
-sql_keywords = ["SELECT", "FROM", "WHERE", "ORDER BY", "AND", "OR", "LIMIT"]
-
-
-class SQLCompleter(Completer):
-    def __init__(self, tables):
-        super().__init__()
-        self.tables = tables
-
-    def get_completions(self, document, complete_event):
-        word = document.get_word_before_cursor()
-
-        if word:
-            for keyword in sql_keywords:
-                if keyword.lower().startswith(word.lower()):
-                    yield Completion(keyword, start_position=-len(word))
-        else:
-            _, __, previous = document.text[:-1].rpartition(" ")
-            if previous.lower() == "from":
-                for tbl in self.tables:
-                    yield Completion(tbl["index"], start_position=0)
-
-
-class SQLLexer(SqlLexer):
-    def get_tokens_unprocessed(self, text, stack=("root",)):
-        for i, typ, val in super().get_tokens_unprocessed(text, stack=stack):
-            if typ == Token.Literal.String.Symbol:
-                yield i, Token.Literal.String.Double, val
-            else:
-                yield i, typ, val
-
-
-class SQLValidator(Validator):
-    def validate(self, document):
-        if not document.text.strip():
-            return
-        text = document.text.rstrip(";")
-        try:
-            result = moz_sql_parser.parse(text)
-            if "from" not in result:
-                raise pyparsing.ParseException(text, len(text), "Expecting from")
-        except pyparsing.ParseException as exc:
-            raise ValidationError(message=str(exc), cursor_position=exc.args[1])
 
 
 def get_query(where):
---
+++ sql.py
@@ -0,0 +1,50 @@
+import moz_sql_parser
+import pyparsing
+
+from prompt_toolkit.validation import Validator, ValidationError
+from prompt_toolkit.completion import Completer, Completion
+from pygments.lexers import SqlLexer
+from pygments.token import Token
+
+sql_keywords = ["SELECT", "FROM", "WHERE", "ORDER BY", "AND", "OR", "LIMIT"]
+
+
+class SQLCompleter(Completer):
+    def __init__(self, tables):
+        super().__init__()
+        self.tables = tables
+
+    def get_completions(self, document, complete_event):
+        word = document.get_word_before_cursor()
+
+        if word:
+            for keyword in sql_keywords:
+                if keyword.lower().startswith(word.lower()):
+                    yield Completion(keyword, start_position=-len(word))
+        else:
+            _, __, previous = document.text[:-1].rpartition(" ")
+            if previous.lower() == "from":
+                for tbl in self.tables:
+                    yield Completion(tbl["index"], start_position=0)
+
+
+class SQLLexer(SqlLexer):
+    def get_tokens_unprocessed(self, text, stack=("root",)):
+        for i, typ, val in super().get_tokens_unprocessed(text, stack=stack):
+            if typ == Token.Literal.String.Symbol:
+                yield i, Token.Literal.String.Double, val
+            else:
+                yield i, typ, val
+
+
+class SQLValidator(Validator):
+    def validate(self, document):
+        if not document.text.strip():
+            return
+        text = document.text.rstrip(";")
+        try:
+            result = moz_sql_parser.parse(text)
+            if "from" not in result:
+                raise pyparsing.ParseException(text, len(text), "Expecting from")
+        except pyparsing.ParseException as exc:
+            raise ValidationError(message=str(exc), cursor_position=exc.args[1])
