bab1bca23d40dbca6dc2e1f200c20c5dd231bd3d
L3viathan <git@l3vi.de>
2018-08-27 21:59:09 +0200

    Version bump (0.0.8)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sql-mojo",
-    version="0.0.7",
+    version="0.0.8",
     install_requires=[
         "sql-mojo-parser",
         "elasticsearch",
